119e3b0596e160699307723d6bfe6d6da3c8a777
kuukuna93 <aku.o.viitanen@gmail.com>
2017-02-05 16:36:30 +0200

    Basic database querys in views.py

--- gameshop/shop/views.py
+++ gameshop/shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q, Count, Sum
 
 # Create your views here.
 
@@ -19,3 +20,34 @@ def login(request):
 
 def logout(request):
 	logout(request)
+
+"""Tarkistetaan onko admin"""
+def list_users(request):
+	order_by = request.GET.get('order_by', 'user')
+	users = UserProfile.objects.all().order_by(order_by)
+	response = TemplateResponse(request, 'admin_userlist.html', {'users': users})
+	response.render()
+	return reponse
+
+def list_games(request):
+	order_by = request.GET.get('order_by', 'name')
+	games = Games.objects.all().order_by(order_by)
+	response = TemplateResponse(request, 'gamelist.html', {'games': games})
+	response.render()
+	return response
+
+"""Tarkistetaan onko kirjautunut"""
+def list_purchased(request, userID="1"):
+	order_by = request.GET.get('order_by', 'game')
+	purchased = Purchased.objects.filter(owner__id=userID).order_by(order_by)
+	response = TemplateResponse(request, 'purchaselist.html', {'purchased': purchased})
+	response.render()
+	return response
+
+
+def list_scores(request, gameID="1"):
+	order_by = request.GET.get('order_by', 'score')
+	scores = Scores.objects.filter(game__id=gameID).order_by(order_by)
+	response = TemplateResponse(request, 'scorelist.html', {'scores': scores})
+	response.render()
+	return response
